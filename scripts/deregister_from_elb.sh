6b45250715e12355c9705722c9ff62adbfcc3143
Oscar Roa <oshardy3@gmail.com>
2016-10-24 10:34:16 -0500

    Half at time instance deployment scripts.

--- scripts/deregister_from_elb.sh
+++ scripts/deregister_from_elb.sh
@@ -4,11 +4,11 @@
 export AWS_DEFAULT_REGION="us-east-2"
 export AUTOSCALING_GROUP_NAME="Infra-LinuxWebASG-1H1XIZ0RK6WKJ"
 export LBN="LinuxWebELB"  # Load Balancer Name
-instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+export instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 # AWS functions
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name $AUTOSCALING_GROUP_NAME --health-check-type EC2
 
-aws elb deregister-instances-from-load-balancer --load-balancer-name $LBN --instances instanceID
+aws elb deregister-instances-from-load-balancer --load-balancer-name $LBN --instances $instanceID
 
 sleep 60
--- scripts/register_with_elb.sh
+++ scripts/register_with_elb.sh
@@ -4,9 +4,9 @@
 export AWS_DEFAULT_REGION="us-east-2"
 export AUTOSCALING_GROUP_NAME="Infra-LinuxWebASG-1H1XIZ0RK6WKJ"
 export LBN="LinuxWebELB"  # Load Balancer Name
-instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+export instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 # AWS functions
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name $AUTOSCALING_GROUP_NAME --health-check-type ELB
 
-aws elb register-instances-with-load-balancer --load-balancer-name $LBN --instances instanceID
+aws elb register-instances-with-load-balancer --load-balancer-name $LBN --instances $instanceID
